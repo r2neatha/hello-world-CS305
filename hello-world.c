16bd2bede77c5ab83c8295fe9d735c28efe04a06
Ryan Todd <rnt45@hwsrv-1244942.hostwindsdns.com>
2024-12-03 22:22:27 +0000

    Change output to Hello world!

--- hello-world.c
+++ hello-world.c
@@ -1,8 +1,10 @@
+
 #include <stdio.h>
 
 int main(void)
 {
-  printf("hello world!\n");]
+  printf("hello world!\n");
+  printf("I am adding Hello World!\n");
 }
 // Adding a comment to hellp world using nano and git!
 // Cycle 2 
